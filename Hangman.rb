0246bdd1d6c9b867dd1480fdd2f75e6b5a4e4bd0
ZeroFries <sgarvagh@uoguelph.ca>
2013-06-10 17:03:00 -0400

    wrote next failing test

--- Hangman.rb
+++ Hangman.rb
@@ -1,14 +1,39 @@
 class Hangman
-	attr_accessor :guesses, :original_word, :chances
+	attr_accessor :guesses, :original_word, :chances, :slate, :word
 
 	def initialize
 		@chances = 8
 		@guesses = []
+		@slate = []
+		@original_word = random_word
+		@word = @original_word
+		@original_word.length.times { @slate << "_" }
 	end
 
 	def random_word
 		words = "Many biological processes involve the conversion of energy into forms that are usable for chemical transformations and are quantum mechanical in nature Such processes involve chemical reactions light absorption formation of excited electronic states transfer of excitation energy and the transfer of electrons and protons hydrogen ions in chemical processes such as photosynthesis and cellular respiration".downcase.split(' ')
 		word = words.sample #randomly select a word
+	end
+
+	def guess_letter(guess)
+		while (guesses.include?(guess) || slate.include?(guess)) #loops until person has fresh guess
+	  	puts "You have already guessed that! Guess again:"
+	  	guess = gets[0].downcase
+	  end
+	  while !("a".."z").include?(guess)
+	    puts "You have to guess a letter! Guess again:"
+	    guess = gets[0].downcase
+	  end
+
+	  if slate.include?(guess)
+      word.count(guess).times do
+       	slate[word.index(guess)] = guess
+       	word = word.sub(guess, ' ') # note: can't use sub! because this modifies the origin object, thus modifing original_word
+      end
+    else
+     	guesses << guess
+     	@chances -= 1
+	  end
 	end
 
 	def win?(slate)
--- tests/hangman_tests.rb
+++ tests/hangman_tests.rb
@@ -2,6 +2,11 @@ require "test/unit"
 require_relative "../Hangman.rb"
 
 class Hangman_tests < Test::Unit::TestCase
+  def setup_guess
+    @hm.original_word = "pie"
+    @hm.word = "pie"
+  end
+
 	def setup
 		@hm = Hangman.new
 		@word = @hm.random_word
@@ -11,22 +16,35 @@ class Hangman_tests < Test::Unit::TestCase
 		assert @word.is_a?(String)
 	end
 
-      def test_initial_chances_is_8
-        assert_equal 8, @hm.chances
-      end
+  def test_initial_chances_is_8 
+    assert_equal 8, @hm.chances
+  end
 
-      def test_initial_guesses_is_an_array
-        assert @hm.guesses.is_a?(Array)
-      end
+  def test_reducing_chances
+    @hm.chances-= 1
+    assert_equal 7, @hm.chances
+  end
 
-      def test_initial_guesses_is_a_empty_array
-        assert @hm.guesses.empty?
-      end
+  def test_initial_guesses_is_an_array
+    assert @hm.guesses.is_a?(Array)
+  end
 
-      def test_incorrect_guess_in_the_array
-        @hm.original_word = "pie"
-        @hm.guess("l")
-        assert @hm.guesses.include?("l")
-      end
+  def test_initial_guesses_is_a_empty_array
+    assert @hm.guesses.empty?
+  end
+
+  def test_incorrect_guess_in_the_array_and_reduces_chances
+    setup_guess
+    @hm.word = "pie"
+    @hm.guess_letter("l")
+    assert @hm.guesses.include?("l")
+    assert_equal @hm.chances, 7
+  end
+
+  def test_correct_guess_adds_to_slate
+    setup_guess
+    @hm.guess_letter("p")
+    assert @hm.slate.include?("p")
+  end
 
 end
