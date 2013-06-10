840e05071505869bdb1af3108808807b5b974644
ZeroFries <sgarvagh@uoguelph.ca>
2013-06-10 16:23:20 -0400

    first test

--- tests/hangman_tests.rb
+++ tests/hangman_tests.rb
@@ -1,10 +1,14 @@
 require "test/unit"
 require_relative "../Hangman.rb"
 
-class Hangman_tests << Test::Unit::TestCase
+class Hangman_tests < Test::Unit::TestCase
 	def setup
 		@hm = Hangman.new
 		@word = @hm.random_word
 	end
+
+	def test_random_word_is_a_string
+		assert @word.is_a?(String)
+	end	
 
 end
