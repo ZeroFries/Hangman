4956ead4cba871bdb7fd8cb9846a7789b895377e
benmliang <benmliang@gmail.com>
2013-06-10 16:42:03 -0400

    add three tests and failed on making a guess

--- tests/hangman_tests.rb
+++ tests/hangman_tests.rb
@@ -9,6 +9,24 @@ class Hangman_tests < Test::Unit::TestCase
 
 	def test_random_word_is_a_string
 		assert @word.is_a?(String)
-	end	
+	end
+
+      def test_initial_chances_is_8
+        assert_equal 8, @hm.chances
+      end
+
+      def test_initial_guesses_is_an_array
+        assert @hm.guesses.is_a?(Array)
+      end
+
+      def test_initial_guesses_is_a_empty_array
+        assert @hm.guesses.empty?
+      end
+
+      def test_incorrect_guess_in_the_array
+        @hm.original_word = "pie"
+        @hm.guess("l")
+        assert @hm.guesses.include?("l")
+      end
 
 end
